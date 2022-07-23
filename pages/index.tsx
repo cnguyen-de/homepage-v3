4f0b85a91448bf4794c4d3cc602b05c6fd65fc50
Chi Nguyen <c.nguyen.prog@gmail.com>
2022-07-24 00:06:44 +0200

    fix next problem requiring importing react

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Avatar from '../components/Avatar'
 import styles from '../styles/Home.module.css'
+import React from 'react'
 
 export default function Home() {
   return (
